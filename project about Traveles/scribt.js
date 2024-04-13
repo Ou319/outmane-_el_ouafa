2b47a3f2fed9ba6f61821e3681a7f1c8f72c55c8
Ou319 <155311386+Ou319@users.noreply.github.com>
2024-04-13 09:56:01 +0200

    web site about Traveles
    using html and css and js

---
+++ project about Traveles/scribt.js
@@ -0,0 +1,8 @@
+const nav=document.getElementById("nav");
+const buttton=document.getElementById("btn");
+const doit=function(){
+    nav.classList.toggle("active");
+    buttton.classList.toggle("active");
+}
+buttton.addEventListener("click",doit)
+console.log("hwdhgfdhr")
